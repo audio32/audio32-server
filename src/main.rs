69f84e0b51431c1974f4826afdff2ec644d36836
Jurek <git@jurek.io>
2025-07-02 15:54:57 -0700

    adding sampling frequency detection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::io;
 use std::net::{SocketAddr, UdpSocket};
 
-
-
 fn main() {
     let addr = "[fe80::a401:1bff:fea2:3f5%10]:50349".parse::<SocketAddr>().unwrap();
     let socket = UdpSocket::bind("[::]:50349").unwrap();
@@ -28,10 +26,35 @@ fn main() {
     let mut out_b: jack::Port<jack::AudioOut> = client
         .register_port("rust_out_r", jack::AudioOut::default())
         .unwrap();
+    const CALC_EVERY: u32 = 512;
+    let mut seq = 0u32;
+    let mut callback = 0;
+    let mut buf_out:Vec<u16> = Vec::with_capacity(1500);
+    let mut last_time = get_time();
+    let mut last_sampling_freq = 0u128;
     let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
         let out_a_p = out_a.as_mut_slice(ps);
+        let buf_size = out_a_p.len();
+        if callback % CALC_EVERY == 0 {
+            let time = get_time();
+            let nanos_per_buffer = time.saturating_sub(last_time);
+            last_time = time;
+            let sampling_freq = 1_000_000_000_000 * buf_size as u128 * CALC_EVERY as u128 / nanos_per_buffer;
+            println!(
+                "ns p buf {}/{} f: {}, delta: {}ppm",
+                nanos_per_buffer,
+                buf_size,
+                sampling_freq,
+                1_000_000 - ((last_sampling_freq * 1_000_000) / sampling_freq) as i64
+            );
+            last_sampling_freq = sampling_freq;
+        }
+        callback = callback.wrapping_add(1);
+
+        // transmission
         for out_a_p in out_a_p.chunks(64) {
-            let mut buf_out:Vec<u16> = Vec::with_capacity(1500);
+            buf_out.clear();
+
             for e in out_a_p.iter().map(|n| (n * u16::MAX as f32) as u16) {
                 for _ in 0..8 {
                     buf_out.push(e);
@@ -41,8 +64,12 @@ fn main() {
             assert!(prefix.is_empty() && suffix.is_empty() &&
                         core::mem::align_of::<u8>() <= core::mem::align_of::<u16>(),
                     "Expected u8 alignment to be no stricter than u16 alignment");
-            println!("len: {}", result.len());
-            let _ = socket.send_to(result, addr).map_err(|e|println!("{:?}", e));
+            //println!("len: {}", result.len());
+            let mut pkg = [0u8;1500];
+            pkg[0..4].copy_from_slice(&seq.to_le_bytes());
+            seq = seq.wrapping_add(1);
+            pkg[4..result.len()+4].copy_from_slice(&result[..]);
+            let _ = socket.send_to(&pkg[..4 + result.len()], addr).map_err(|e|println!("{:?}", e));
         }
         let out_b_p = out_b.as_mut_slice(ps);
         let in_a_p = in_a.as_slice(ps);
@@ -59,10 +86,26 @@ fn main() {
     // 4. Wait for user input to quit
     println!("Press enter/return to quit...");
     let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input).ok();
+    let socket = UdpSocket::bind("[::]:50348").unwrap();
+    loop {
+        io::stdin().read_line(&mut user_input).ok();
+        let _ = socket.send_to(&[0,0,0,0, 0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1], addr).map_err(|e|println!("{:?}", e));
+    }
 
     // 5. Not needed as the async client will cease processing on `drop`.
     if let Err(err) = active_client.deactivate() {
         eprintln!("JACK exited with error: {err}");
     }
+}
+
+
+fn get_time() -> u128 {
+    let mut timespec = libc::timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
+    let res = unsafe {libc::clock_gettime(libc::CLOCK_REALTIME, &mut timespec) };
+    assert_eq!(res, 0, "Could not get libc::clock_gettime(libc::CLOCK_REALTIME, /*...*/)");
+
+    timespec.tv_nsec as u128 + timespec.tv_sec as u128 * 1_000_000_000u128
 }
