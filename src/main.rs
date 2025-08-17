1b48cbba5e7712078e0f01527596560df3c74330
Jurek <git@jurek.io>
2025-08-17 11:53:13 -0700

    try this again

--- src/main.rs
+++ src/main.rs
@@ -78,7 +78,7 @@ fn jack_client(sender: Sender<(Vec<u8>, u32)>) {
     let mut last_time = get_time();
     let mut last_sampling_freq = 0u128;
     // new
-    let mut times = vec![(0, 0, 0, 0); 1024 * 8];
+    let mut times = vec![(0, 0, 0); 1024 * 16];
     let mut prev_time = get_time();
     let mut i = 0;
 
@@ -94,11 +94,11 @@ fn jack_client(sender: Sender<(Vec<u8>, u32)>) {
         //println!("{}", (time as i128 / 1000 - jack_time as i128));
         let cycle_times = ps.cycle_times().unwrap();
 
-        let callback_late = (jack_time as i128 - cycle_times.current_usecs as i128) * 1000;
-        // jack time = 0 in ptp time
+        let callback_late = jack_time as i128 - cycle_times.current_usecs as i128;
+
         let ptp_jack_diff = time as i128 - (jack_time as i128 * 1_000);
 
-        let ptp_start_time = time as i128 + ptp_jack_diff + callback_late;
+        let ptp_start_time = time as i128 + ptp_jack_diff;
         let ptp_start_time_frames = cycle_times.current_frames;
 
         /*println!(
@@ -110,8 +110,9 @@ fn jack_client(sender: Sender<(Vec<u8>, u32)>) {
             callback_late,
         );*/
 
+        let time_frames = ps.frames_since_cycle_start();
         let len = times.len();
-        times[i as usize % len] = (ptp_start_time, ptp_start_time_frames, time, callback_late);
+        times[i as usize % len] = (ptp_start_time, ptp_start_time_frames, callback_late);
         if i % (len as u32) == 0 {
             println!("{:?}", times);
         }
